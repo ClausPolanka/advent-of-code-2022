26f92306ad9b2586a9299f8080a20f41f34360c7
Claus Polanka <claus.polanka@chello.at>
2022-12-05 00:20:47 +0100

    day03 - Improve names

--- src/main/kotlin/adventofcode/day03/Main.kt
+++ src/main/kotlin/adventofcode/day03/Main.kt
@@ -3,31 +3,39 @@ package adventofcode.day03
 import java.io.*
 
 fun main() {
-    val score = mutableMapOf<Char, Int>()
-    CharRange('a', 'z').forEachIndexed { idx, c -> score[c] = idx + 1 }
-    IntRange(27, 52).forEachIndexed { idx, nr -> score['A' + idx] = nr }
+    val scoreMap = mutableMapOf<Char, Int>()
+    CharRange('a', 'z').forEachIndexed { idx, c -> scoreMap[c] = idx + 1 }
+    IntRange(27, 52).forEachIndexed { idx, nr -> scoreMap['A' + idx] = nr }
 
     val input1 = File("requirements/day03/input.txt")
         .useLines { it.toList() }
-    val sumOfPriorities1 = part1(input1, score)
+    val sumOfPriorities1 = part1(input1, scoreMap)
     println("Part 1 sum of priorities: $sumOfPriorities1")
 
     val input2 = File("requirements/day03/input_part2.txt")
         .useLines { it.toList() }
-    val sumOfPriorities2 = part2(input2, score)
+    val sumOfPriorities2 = part2(input2, scoreMap)
     println("Part 2 sum of priorities: $sumOfPriorities2")
 }
 
-private fun part1(input1: List<String>, score: MutableMap<Char, Int>): Int =
-    input1.map { it.chunked(it.length / 2) }
-        .map { it.map { it.toList() } }
-        .flatMap { it[0].intersect(it[1]) }
-        .map { score[it] }
-        .sumOf { it!! }
+private fun part1(
+    rucksacks: List<String>,
+    scoreMap: MutableMap<Char, Int>): Int =
+    rucksacks.map { rucksack -> rucksack.chunked(rucksack.length / 2) }
+        .map { compartments -> compartments.map { c -> c.toList() } }
+        .flatMap { compartment -> compartment[0].intersect(compartment[1]) }
+        .map { item -> scoreMap[item] }
+        .sumOf { score -> score!! }
 
-private fun part2(input1: List<String>, score: MutableMap<Char, Int>): Int =
-    input1.chunked(3)
-        .map { it.map { it.toList() } }
-        .flatMap { it[0].intersect(it[1]).intersect(it[2]) }
-        .map { score[it] }
-        .sumOf { it!! }
+private fun part2(
+    rucksacks: List<String>,
+    scoreMap: MutableMap<Char, Int>): Int =
+    rucksacks.chunked(3)
+        .map { rucksackGroup ->
+            rucksackGroup.map { rucksack -> rucksack.toList() }
+        }
+        .flatMap { rucksack ->
+            rucksack[0].intersect(rucksack[1]).intersect(rucksack[2])
+        }
+        .map { item -> scoreMap[item] }
+        .sumOf { score -> score!! }
