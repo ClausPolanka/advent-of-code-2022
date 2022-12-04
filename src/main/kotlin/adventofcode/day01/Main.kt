a12c58d07f1d0eac3b30854ee0633170594563d3
Claus Polanka <claus.polanka@chello.at>
2022-12-04 15:15:55 +0100

    day01 - Add solution for first part and receive first gold star

--- src/main/kotlin/adventofcode/day01/Main.kt
+++ src/main/kotlin/adventofcode/day01/Main.kt
@@ -4,5 +4,19 @@ import java.io.*
 
 fun main() {
     val input = File("requirements/day01/input.txt").useLines { it.toList() }
-    println(input)
+    println(maxOf(input))
+}
+
+fun maxOf(input: List<String>): Int? {
+    val all = mutableListOf<List<Int>>()
+    var group = mutableListOf<Int>()
+    input.forEach {
+        if (it.isNotEmpty()) {
+            group.add(it.toInt())
+        } else {
+            all.add(group)
+            group = mutableListOf()
+        }
+    }
+    return all.maxOfOrNull { it.sum() }
 }
--- src/test/kotlin/tests/adventofcode/day01/Tests.kt
+++ src/test/kotlin/tests/adventofcode/day01/Tests.kt
@@ -1,10 +1,32 @@
 package tests.adventofcode.day01
 
+import adventofcode.day01.*
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.lang.System.lineSeparator
 
 class Tests {
+
     @Test
-    fun name() {
-        TODO("Not yet implemented")
+    fun calculateMaxForSampleInput() {
+        val input = """
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+        """.trimIndent().split(lineSeparator())
+        val max = maxOf(input)
+        assertEquals(24000, max)
     }
+
 }
