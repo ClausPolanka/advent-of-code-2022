d445d225cd6af6fb1e4b837b70a1d0ff5e4dfea0
Claus Polanka <claus.polanka@chello.at>
2022-12-05 11:52:36 +0100

    day04 - Add solution for part 2

--- src/main/kotlin/adventofcode/day04/Main.kt
+++ src/main/kotlin/adventofcode/day04/Main.kt
@@ -3,10 +3,12 @@ package adventofcode.day04
 import java.io.*
 
 fun main() {
-    val input1 = File("requirements/day04/input.txt")
+    val input = File("requirements/day04/input.txt")
         .useLines { it.toList() }
     println("Part 1 Assignment Pairs where one range fully contains the other: "
-            + part1(input1))
+            + part1(input))
+    println("Part 2 Assignment pairs where ranges overlap: "
+            + part2(input))
 }
 
 private fun part1(input1: List<String>) = input1
@@ -21,4 +23,18 @@ private fun part1(input1: List<String>) = input1
     .map {
         listOf(it[0].containsAll(it[1]), it[1].containsAll(it[0]))
     }.filter { it.contains(true) }
+    .size
+
+private fun part2(input1: List<String>) = input1
+    .map { it.split(",") }
+    .map {
+        it.map {
+            IntRange(
+                it.split("-")[0].toInt(),
+                it.split("-")[1].toInt())
+        }
+    }.map { it.map { it.toList() } }
+    .map {
+        it[0].intersect(it[1])
+    }.filter { it.isNotEmpty() }
     .size
