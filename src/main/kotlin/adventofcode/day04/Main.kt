8c6863256c39f5870513d4b8d3d93f94b5afce25
Claus Polanka <claus.polanka@chello.at>
2022-12-05 12:24:54 +0100

    day04 - Kotlinfy

--- src/main/kotlin/adventofcode/day04/Main.kt
+++ src/main/kotlin/adventofcode/day04/Main.kt
@@ -2,6 +2,8 @@ package adventofcode.day04
 
 import java.io.*
 
+typealias SectionIdRange = List<Int>
+
 fun main() {
     val input = File("requirements/day04/input.txt")
         .useLines { it.toList() }
@@ -11,30 +13,31 @@ fun main() {
             + part2(input))
 }
 
-private fun part1(input1: List<String>) = input1
-    .map { it.split(",") }
-    .map {
-        it.map {
-            IntRange(
-                it.split("-")[0].toInt(),
-                it.split("-")[1].toInt())
-        }
-    }.map { it.map { it.toList() } }
-    .map {
-        listOf(it[0].containsAll(it[1]), it[1].containsAll(it[0]))
-    }.filter { it.contains(true) }
+private fun part1(input1: List<String>) = sectionIdRanges(input1)
+    .findAssignmentPairsFullyContainingOthers()
     .size
 
-private fun part2(input1: List<String>) = input1
-    .map { it.split(",") }
-    .map {
-        it.map {
-            IntRange(
-                it.split("-")[0].toInt(),
-                it.split("-")[1].toInt())
-        }
-    }.map { it.map { it.toList() } }
-    .map {
-        it[0].intersect(it[1])
-    }.filter { it.isNotEmpty() }
+private fun part2(input1: List<String>) = sectionIdRanges(input1)
+    .findSectionIdRangeOverlaps()
     .size
+
+/** ["2-4,6-8", "2-3,4-5"] => [[[2,3,4], [6,7,8]], [[2,3], [4,5]]] */
+private fun sectionIdRanges(input: List<String>): List<List<SectionIdRange>> =
+    input
+        .map { it.split(",") } // "2-4,6-8" =>  ["2-4", "6-8"]
+        .map { raingeStrPairs -> // ["2-4", "6-8"] => [2..4, 6..8]
+            raingeStrPairs.map { rangeStr ->
+                val rangeBorders = rangeStr.split("-")
+                IntRange(rangeBorders[0].toInt(), rangeBorders[1].toInt())
+            }
+        }
+        // [2..4, 6..8] => [[2,3,4], [6,7,8]]
+        .map { rangePairs -> rangePairs.map { range -> range.toList() } }
+
+fun List<List<SectionIdRange>>.findAssignmentPairsFullyContainingOthers() =
+    map { listOf(it[0].containsAll(it[1]), it[1].containsAll(it[0])) }
+        .filter { it.contains(true) }
+
+fun List<List<SectionIdRange>>.findSectionIdRangeOverlaps() =
+    map { it[0].intersect(it[1].toSet()) }
+        .filter { it.isNotEmpty() }
