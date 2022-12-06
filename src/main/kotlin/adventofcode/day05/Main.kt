67e9e0658233bb31d16b2528cebfff95bce5d4bd
Claus Polanka <claus.polanka@chello.at>
2022-12-06 18:59:33 +0100

    day05 - Add solution for part 2

--- src/main/kotlin/adventofcode/day05/Main.kt
+++ src/main/kotlin/adventofcode/day05/Main.kt
@@ -5,7 +5,7 @@ import java.io.*
 fun main() {
     val input = File("requirements/day05/input.txt").readText()
         .split("${System.lineSeparator()}${System.lineSeparator()}")
-    val result = part1(input)
+    val result = part2(input)
     println(result)
 }
 
@@ -46,6 +46,50 @@ private fun part1(input: List<String>): String {
         }
     }
     return stacks.joinToString(separator = "") { it.last() }
+}
+
+private fun part2(input: List<String>): String {
+    val stacksOfCrates = input[0]
+
+    val nrOfStacks = stacksOfCrates.lines()
+        .last()
+        .split(" ")
+        .filter { it.isNotBlank() }
+        .map { it.toInt() }
+        .size
+
+    val stackLines = stacksOfCrates.lines()
+        .dropLast(1)
+        .map { Pair(it, indicesFor(it.length - 1)) }
+        .map { pair ->
+            pair.second.map { idx ->
+                val vcont = pair.first[idx].toString()
+                vcont.ifBlank { "" }
+            }
+        }
+        .reversed()
+
+    val stacks = stackRows(nrOfStacks, stackLines)
+    val moves = input[1].lines()
+        .map { it.split(" ") }
+        .filter { it.size == 6 }
+        .map {
+            Move(
+                quantity = it[1].toInt(),
+                from = it[3].toInt() - 1,
+                to = it[5].toInt() - 1)
+        }
+    moves.forEach { m ->
+        val toMove = stacks[m.from].takeLast(m.quantity)
+        repeat(m.quantity) {
+            stacks[m.from].removeLast()
+        }
+        stacks[m.to].addAll(toMove)
+    }
+
+    return stacks
+        .filter { it.isNotEmpty() }
+        .joinToString(separator = "") { it.last() }
 }
 
 private fun stackRows(nrOfStacks: Int, stackLines: List<List<String>>) =
