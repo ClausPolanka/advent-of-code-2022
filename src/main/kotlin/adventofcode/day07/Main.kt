dbe2601328523c0c9a100d02636ca4b4d50516ed
Claus Polanka <claus.polanka@chello.at>
2022-12-08 19:34:49 +0100

    day07 - Add sample

--- src/main/kotlin/adventofcode/day07/Main.kt
+++ src/main/kotlin/adventofcode/day07/Main.kt
@@ -3,6 +3,6 @@ package adventofcode.day07
 import java.io.*
 
 fun main() {
-    val input = File("requirements/day06/input.txt").readText()
+    val input = File("requirements/day07/sample.txt").readText()
     println(input)
 }
