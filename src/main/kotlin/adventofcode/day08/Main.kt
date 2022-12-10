18853d7f2537840c865e23d747669991770708f3
Claus Polanka <claus.polanka@chello.at>
2022-12-10 20:57:45 +0100

    day08 - Add scenic score function

--- src/main/kotlin/adventofcode/day08/Main.kt
+++ src/main/kotlin/adventofcode/day08/Main.kt
@@ -13,18 +13,27 @@ fun main() {
     val lines = sampleInput.split(System.lineSeparator())
     val inputLines =
         File("requirements/day08/input.txt").useLines { it.toList() }
-    val sampleResult = part1(lines)
-    val inputResult = part1(inputLines)
+    val treesSample = treesFor(lines)
+    val treesInput = treesFor(inputLines)
+    val sampleResult = part1(treesSample)
+    val inputResult = part1(treesInput)
     // sample => 21, input => 1695
     println("Part 1 Visible trees: '" + sampleResult.distinct().size + "'")
     println("Part 1 Visible trees: '" + inputResult.distinct().size + "'")
+    println()
+//
+//    sampleResult
+//        .filter { it.split(",")[0].toInt() > 0 }
+//        .filter { it.split(",")[1].toInt() > 0 }
+//        .filter { it.split(",")[0].toInt() < treesSample.size - 1 }
+//        .filter { it.split(",")[1].toInt() < treesSample.size - 1 }
+//        .sorted()
+//        .groupBy { it }.forEach {
+//            println(it.key + " => " + it.value.size)
+//        }
 }
 
-private fun part1(inputLines: List<String>): List<String> {
-    val trees = inputLines
-        .filter { it.isNotEmpty() }
-        .map { it.toList().map { tree -> tree.digitToInt() } }
-
+private fun part1(trees: List<List<Int>>): List<String> {
     val treesVisibleInRows = trees.flatMapIndexed { rowIndex, _ ->
         rightNeighbours(trees[rowIndex], rowIndex) +
                 leftNeighbours(trees[rowIndex], rowIndex)
@@ -36,6 +45,12 @@ private fun part1(inputLines: List<String>): List<String> {
     return treesVisibleInRows + treesVisibleInColumns
 }
 
+private fun treesFor(inputLines: List<String>): List<List<Int>> {
+    return inputLines
+        .filter { it.isNotEmpty() }
+        .map { it.toList().map { tree -> tree.digitToInt() } }
+}
+
 private fun rightNeighbours(
     trees: List<Int>,
     index: Int,
@@ -45,9 +60,18 @@ private fun rightNeighbours(
         val isHighest =
             if (neighbours.isEmpty()) true else tree > neighbours.max()
         if (isHighest) {
+            val scenicScore: Int =
+                scenicScoreFor(tree, neighbours, treeIndex, index)
             if (isRow) "$index,$treeIndex" else "$treeIndex,$index"
         } else ""
     }.filter { it.isNotEmpty() }
+}
+
+fun scenicScoreFor(tree: Int, neighbours: List<Int>, treeIndex: Int, index: Int): Int {
+    if (treeIndex == 0 || index == 0) {
+        return 0
+    }
+    return neighbours.takeWhile { tree <= it }.size
 }
 
 private fun leftNeighbours(
@@ -56,12 +80,15 @@ private fun leftNeighbours(
     isRow: Boolean = true): List<String> {
     return trees.mapIndexed { treeIndex, tree ->
         if (treeIndex == 0) {
+            val scenicScore: Int = 0
             if (isRow) "$index,0" else "0,$index"
         } else {
             val neighbours = trees.subList(0, treeIndex)
             val isHighest =
                 if (neighbours.isEmpty()) true else tree > neighbours.max()
             if (isHighest) {
+                val scenicScore: Int =
+                    scenicScoreFor(tree, neighbours, treeIndex, index)
                 if (isRow) "$index,$treeIndex" else "$treeIndex,$index"
             } else ""
         }
