8e7fedcedbc06759fb49ba3f9332e8b3271d9391
Claus Polanka <claus.polanka@chello.at>
2022-12-20 13:42:03 +0100

    day09 - Support moving to the right and left same row

--- src/main/kotlin/adventofcode/day09/Main.kt
+++ src/main/kotlin/adventofcode/day09/Main.kt
@@ -1,5 +1,7 @@
 package adventofcode.day09
 
+import kotlin.math.*
+
 fun main() {
     val grid = """
         ......
@@ -23,4 +25,44 @@ fun main() {
         .map { row -> row.split(" ") }
 
     commands.forEach { println(it) }
+
+    var head = Pair(0, 0)
+    var tail = Pair(0, 0)
+
+
+    // R4
+    IntRange(1, 4).forEach {
+        head = Pair(head.first, it)
+        println("H: $head")
+        tail = updateTail(tail, head)
+        println("T: $tail")
+    }
+
+    // L4
+    IntRange(1, 4).reversed().forEach {
+        head = Pair(head.first, it)
+        println("H: $head")
+        tail = updateTail(tail, head)
+        println("T: $tail")
+    }
+}
+
+fun updateTail(tail: Pair<Int, Int>, head: Pair<Int, Int>): Pair<Int, Int> {
+
+    // Do nothing when distance is max 1
+    if (abs(head.first - tail.first) <= 1 && abs(head.second - tail.second) <= 1) {
+        return tail
+    }
+
+    // Move R
+    if (head.first == tail.first && abs(head.second - tail.second) > 1 && head.second > tail.second) {
+        return Pair(tail.first, tail.second + 1)
+    }
+
+    // Move L
+    if (head.first == tail.first && abs(head.second - tail.second) > 1 && head.second < tail.second) {
+        return Pair(tail.first, tail.second - 1)
+    }
+
+    return Pair(-1, -1)
 }
