bd848117657f0a3969cd77e7a26898d181436c14
Claus Polanka <claus.polanka@chello.at>
2022-12-20 18:26:39 +0100

    day09 - Simplify and rename

--- src/main/kotlin/adventofcode/day09/Main.kt
+++ src/main/kotlin/adventofcode/day09/Main.kt
@@ -40,7 +40,7 @@ fun main() {
 
     println()
     println("How many positions does the tail of the rope visit at least once?")
-    println(moves.size)
+    println(visited.size) // Part 1: 5683 (input) Part 1: 13 (sample)
 }
 
 private fun moveRight(moves: Int): Pair<Pair<Int, Int>, Pair<Int, Int>> {
@@ -51,6 +51,7 @@ private fun moveRight(moves: Int): Pair<Pair<Int, Int>, Pair<Int, Int>> {
         println(" => $head")
         print("T: $tail")
         tail = updateTail(tail, head)
+        visited.add(tail)
         println(" => $tail")
     }
     return Pair(head, tail)
@@ -64,6 +65,7 @@ private fun moveLeft(moves: Int): Pair<Pair<Int, Int>, Pair<Int, Int>> {
         println(" => $head")
         print("T: $tail")
         tail = updateTail(tail, head)
+        visited.add(tail)
         println(" => $tail")
     }
     return Pair(head, tail)
@@ -77,6 +79,7 @@ private fun moveDown(moves: Int): Pair<Pair<Int, Int>, Pair<Int, Int>> {
         println(" => $head")
         print("T: $tail")
         tail = updateTail(tail, head)
+        visited.add(tail)
         println(" => $tail")
     }
     return Pair(head, tail)
@@ -90,107 +93,82 @@ private fun moveUp(moves: Int) {
         println(" => $head")
         print("T: $tail")
         tail = updateTail(tail, head)
+        visited.add(tail)
         println(" => $tail")
     }
 }
 
-val moves = mutableSetOf<Pair<Int, Int>>()
+val visited = mutableSetOf<Pair<Int, Int>>()
 
 fun updateTail(tail: Pair<Int, Int>, head: Pair<Int, Int>): Pair<Int, Int> {
 
     // Do nothing when distance is max 1
     if (abs(head.first - tail.first) <= 1 && abs(head.second - tail.second) <= 1) {
-        val t = Pair(tail.first, tail.second)
-        moves.add(t)
-        return t
+        return Pair(tail.first, tail.second)
     }
 
     // Move R
     if (head.first == tail.first && abs(head.second - tail.second) > 1 && head.second > tail.second) {
-        val t = Pair(tail.first, tail.second + 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first, tail.second + 1)
     }
 
     // Move L
     if (head.first == tail.first && abs(head.second - tail.second) > 1 && head.second < tail.second) {
-        val t = Pair(tail.first, tail.second - 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first, tail.second - 1)
     }
 
     // Move U
     if (head.second == tail.second && abs(head.first - tail.first) > 1 && head.first > tail.first) {
-        val t = Pair(tail.first + 1, tail.second)
-        moves.add(t)
-        return t
+        return Pair(tail.first + 1, tail.second)
     }
 
     // Move D
     if (head.second == tail.second && abs(head.first - tail.first) > 1 && head.first < tail.first) {
-        val t = Pair(tail.first - 1, tail.second)
-        moves.add(t)
-        return t
+        return Pair(tail.first - 1, tail.second)
     }
 
     // ROWS
 
     // Move diagonal up/right
     if (abs(head.first - tail.first) == 2 && head.first > tail.first && head.second > tail.second) {
-        val t = Pair(tail.first + 1, tail.second + 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first + 1, tail.second + 1)
     }
 
     // Move diagonal down/right
     if (abs(head.first - tail.first) == 2 && head.first < tail.first && head.second > tail.second) {
-        val t = Pair(tail.first - 1, tail.second + 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first - 1, tail.second + 1)
     }
 
     // Move diagonal down/left
     if (abs(head.first - tail.first) == 2 && head.first < tail.first && head.second < tail.second) {
-        val t = Pair(tail.first - 1, tail.second - 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first - 1, tail.second - 1)
     }
 
     // Move diagonal up/left
     if (abs(head.first - tail.first) == 2 && head.first > tail.first && head.second < tail.second) {
-        val t = Pair(tail.first + 1, tail.second - 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first + 1, tail.second - 1)
     }
 
     // Cols
 
     // Up/Right
     if (abs(head.second - tail.second) == 2 && head.second > tail.second && head.first > tail.first) {
-        val t = Pair(tail.first + 1, tail.second + 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first + 1, tail.second + 1)
     }
 
     // Up/Left
     if (abs(head.second - tail.second) == 2 && head.second < tail.second && head.first > tail.first) {
-        val t = Pair(tail.first + 1, tail.second - 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first + 1, tail.second - 1)
     }
 
     // Down/Left
     if (abs(head.second - tail.second) == 2 && head.second < tail.second && head.first < tail.first) {
-        val t = Pair(tail.first - 1, tail.second - 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first - 1, tail.second - 1)
     }
 
     // Down/Right
     if (abs(head.second - tail.second) == 2 && head.second > tail.second && head.first < tail.first) {
-        val t = Pair(tail.first - 1, tail.second + 1)
-        moves.add(t)
-        return t
+        return Pair(tail.first - 1, tail.second + 1)
     }
 
     return Pair(-1, -1)
