a8da58139f685fb48e2939caa1b62751bb1cb782
Claus Polanka <claus.polanka@chello.at>
2022-12-26 19:42:51 +0100

    day11 - Add parseMonkey function which supports id, starting items and * operation

--- src/main/kotlin/adventofcode/day11/Main.kt
+++ src/main/kotlin/adventofcode/day11/Main.kt
@@ -14,19 +14,57 @@ fun main() {
 }
 
 fun parseMonkey(s: String): Monkey {
-    return defaultMoney
+    if (s.isEmpty()) {
+        return defaultMonkey
+    }
+    val lines = s.split(System.lineSeparator())
+    lines.forEach {
+        println(it)
+    }
+    val monkeyId: Int = parseMonkeyId(lines[0])
+    val startingItems: List<Int> = parseStartingItems(lines[1])
+    val operation: Operation = parseOperation(lines[2])
+    return defaultMonkey.copy(
+        id = monkeyId,
+        startingItems = startingItems,
+        fn = operation
+    )
+}
+
+fun parseOperation(s: String): Operation {
+    val op = s.split("=")[1].trim()
+    if (op.contains("*")) {
+        val left = op.split("*")[0].trim()
+        val right = op.split("*")[1].trim()
+        return Operation(left, right, "*")
+    }
+    return Operation("left", "right", "-")
+}
+
+data class Operation(val left: String, val right: String, val s: String)
+
+fun parseMonkeyId(s: String): Int {
+    return s.split(" ")[1].split(":").first().toInt()
+}
+
+fun parseStartingItems(s: String): List<Int> {
+    return s.split(":")[1].split(",")
+        .map { it.trim() }
+        .map { it.toInt() }
 }
 
 data class Monkey(
+    val id: Int,
     val startingItems: List<Int>,
-    val fn: (Int, Int) -> Int,
+    val fn: Operation,
     val predicate: (Int, Int) -> Boolean,
     val trueMonkeyId: Int,
     val falseMonkeyId: Int)
 
-val defaultMoney = Monkey(
+val defaultMonkey = Monkey(
+    id = -1,
     startingItems = emptyList(),
-    fn = { _, _ -> -1 },
+    fn = Operation("x", "y", "+"),
     predicate = { _, _ -> false },
     trueMonkeyId = -1,
     falseMonkeyId = -1)
--- src/test/kotlin/tests/adventofcode/day11/Tests.kt
+++ src/test/kotlin/tests/adventofcode/day11/Tests.kt
@@ -9,8 +9,30 @@ class Tests {
     fun `parse monkey returns default monkey for empty string`() {
         val m = parseMonkey("")
         assertEquals(
-            defaultMoney,
+            defaultMonkey,
             m,
             "monkey")
+    }
+
+    @Test
+    fun `parse monkey returns monkey for input string`() {
+        val s = """
+            Monkey 0:
+              Starting items: 79, 98
+              Operation: new = old * 19
+              Test: divisible by 23
+                If true: throw to monkey 2
+                If false: throw to monkey 3
+        """.trimIndent()
+        val m = parseMonkey(s)
+        // @formatter:off
+        assertAll(
+            { assertEquals(0, m.id, "monkey id") },
+            { assertEquals(
+                    listOf(79, 98), m.startingItems, "monkey starting items") },
+            { assertEquals(
+                Operation("old", "19", "*"), m.fn, "monkey operation") },
+        )
+        // @formatter:on
     }
 }
