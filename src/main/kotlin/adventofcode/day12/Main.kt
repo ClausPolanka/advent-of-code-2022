7a05a9bbd5fbc9eccda1ee913d2fd779cdeef201
Claus Polanka <claus.polanka@chello.at>
2023-01-02 13:12:06 +0100

    day12 - part 1: Support basic neighbour detection (without error handling)

--- src/main/kotlin/adventofcode/day12/Main.kt
+++ src/main/kotlin/adventofcode/day12/Main.kt
@@ -13,14 +13,43 @@ fun main() {
             acctuvwj
             abdefghi
         """.trimIndent()
-    val grid = heightMap.split(lineSeparator())
-        .map { it.toList() }
+    val grid = heightMap.toGrid()
 
-    val x: Int = 1
-    val y: Int = 1
+    val row: Int = 1
+    val column: Int = 1
     val neighbours = grid.neighboursOf(1, 1)
 }
 
-fun <E> List<E>.neighboursOf(x: Int, y: Int): List<Char> {
-    TODO("Not yet implemented")
+fun String.toGrid(): List<List<Char>> = split(lineSeparator())
+    .map { it.toList() }
+
+fun <E> List<List<E>>.neighboursOf(row: Int, column: Int): List<Cell> {
+    val leftNeighbour = Cell(
+        value = this[row][column - 1].toString(),
+        row = row,
+        column = column - 1,
+        name = "leftNeighbour")
+    val belowNeighbour = Cell(
+        value = this[row + 1][column].toString(),
+        row = row + 1,
+        column = column,
+        name = "belowNeighbour")
+    val rightNeighbour = Cell(
+        value = this[row][column + 1].toString(),
+        row = row,
+        column = column + 1,
+        name = "rightNeighbour")
+    val aboveNeighbour = Cell(
+        value = this[row - 1][column].toString(),
+        row = row - 1,
+        column = column,
+        name = "aboveNeighbour")
+    return listOf(
+        leftNeighbour,
+        belowNeighbour,
+        rightNeighbour,
+        aboveNeighbour)
 }
+
+data class Cell(val value: String, val row: Int, val column: Int,
+                val name: String)
--- src/test/kotlin/tests/adventofcode/day12/Tests.kt
+++ src/test/kotlin/tests/adventofcode/day12/Tests.kt
@@ -1,4 +1,24 @@
 package tests.adventofcode.day12
 
+import adventofcode.day12.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+
 class Tests {
+    @Test
+    fun `return cell's neighbours in a 3x3 grid`() {
+        val grid: List<List<Char>> = """
+            ada
+            aac
+            aba
+        """.trimIndent().toGrid()
+        val neighbours = grid.neighboursOf(1, 1)
+        assertEquals(listOf(
+            Cell(value = "a", row = 1, column = 0, name = "leftNeighbour"),
+            Cell(value = "b", row = 2, column = 1, name = "belowNeighbour"),
+            Cell(value = "c", row = 1, column = 2, name = "rightNeighbour"),
+            Cell(value = "d", row = 0, column = 1, name = "aboveNeighbour"),
+        ), neighbours, "neighbours")
+    }
 }
+
