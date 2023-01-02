f233f6a08334200cacd668ab524edfccfb80eba8
Claus Polanka <claus.polanka@chello.at>
2023-01-02 12:25:53 +0100

    day12 - part 1: Sketch out behaviour to detect cells neighbours

--- src/main/kotlin/adventofcode/day12/Main.kt
+++ src/main/kotlin/adventofcode/day12/Main.kt
@@ -1,9 +1,26 @@
 package adventofcode.day12
 
 import java.io.*
+import java.lang.System.lineSeparator
 
 fun main() {
     val input =
         File("requirements/day12/sample.txt").readText()
-    println(input)
+    val heightMap = """
+            Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi
+        """.trimIndent()
+    val grid = heightMap.split(lineSeparator())
+        .map { it.toList() }
+
+    val x: Int = 1
+    val y: Int = 1
+    val neighbours = grid.neighboursOf(1, 1)
+}
+
+private fun <E> List<E>.neighboursOf(x: Int, y: Int): List<Char> {
+    TODO("Not yet implemented")
 }
